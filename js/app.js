0332abafb41dbd731bd428430c1ebe5d3db41c05
SOHELMAHMUD12 <sohelcse123@gmail.com>
2022-08-21 00:52:55 +0600

    added calculator

---
+++ js/app.js
@@ -0,0 +1,56 @@
+function getPin(){
+    const pin = generatePin();
+    const pinString = pin + '';
+    if(pinString.length === 4){
+        return pin;
+    }
+    else{
+        // console.log('pin not a 3 digit found', pin);
+        return getPin();
+    }
+
+}
+function generatePin(){
+    const random = Math.round(Math.random()*10000);
+    return random;
+}
+
+document .getElementById('generate-pin').addEventListener('click', function(){
+    const pin = getPin();
+    // console.log(pin);
+
+    // display pin
+
+    const displayPinField = document.getElementById('display-pin');
+    displayPinField.value = pin;
+
+})
+
+document.getElementById('calculator').addEventListener('click', function(event){
+    const number = event.target.innerText;
+    const typeNumberField = document.getElementById('typed-numbers');
+    const previousTypedNumber = typeNumberField.value;
+    if(isNaN(number)){
+        if(number === 'C'){
+            typeNumberField.value = '';
+        }
+        else if(number === '<'){
+            const digits = previousTypedNumber.split('')
+            digits.pop();
+            const remainingDigits = digits.join('');
+            typeNumberField.value = remainingDigits;
+
+        }
+        // console.log(number);  
+
+    }
+    else{
+   
+       
+       
+      
+        const newTypedNumber = previousTypedNumber + number;
+        typeNumberField.value = newTypedNumber;
+    }
+      
+})
